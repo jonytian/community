4a099b49e6e456c47b3597a30908bdb676cc8812
tyj <yaojiang.tian@langlangit.com>
2018-11-27 17:08:11 +0800

    新项目开始了

---
+++ src/main/java/com/tyj/community/CommunityApplication.java
@@ -0,0 +1,14 @@
+package com.tyj.community;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.tyj.community.dao")
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+}
---
+++ src/main/java/com/tyj/community/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.tyj.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tyj on 2018/11/27.
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    String index(){
+        return "index";
+    }
+}
